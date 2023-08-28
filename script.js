07c9c4c6a427f9aa8d62aade6f25f9f81d208312
Pawan123Pawan <105289150+Pawan123Pawan@users.noreply.github.com>
2023-08-29 03:37:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+//your JS code here. If r;
+const output = document.getElementById("output");
+      const button = document.getElementById("btn");
+
+      button.addEventListener("click", (e) => {
+          e.preventDefault();
+          console.log("djs");
+          const text = document.getElementById("text").value;
+          const delay = Number(document.getElementById("delay").value); // Corrected the typo here
+
+          if (text && delay > 0) { // Corrected variable name here
+              call(text, delay); // Corrected variable name here
+          }
+      });
+
+      async function call(message, delay) {
+          await new Promise((resolve) => {
+              setTimeout(() => {
+                  resolve();
+              }, delay);
+          });
+          output.innerText = message;
+      }
